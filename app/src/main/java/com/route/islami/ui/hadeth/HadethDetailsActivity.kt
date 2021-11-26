673816ff972d84f88938a19bc5ec1b7344637ec5
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2021-11-26 14:41:47 +0200

    merged with development and resolved imports issues

--- app/src/main/java/com/route/islami/ui/hadeth/HadethDetailsActivity.kt
+++ app/src/main/java/com/route/islami/ui/hadeth/HadethDetailsActivity.kt
@@ -1,7 +1,8 @@
 package com.route.islami.ui.hadeth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.route.islami.R
 
 class HadethDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
